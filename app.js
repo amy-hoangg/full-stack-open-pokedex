30db389346c066b0fd79178836f7fe300c0a603f
Amy Hoang <my.hoang@tuni.fi>
2023-07-26 18:33:48 +0300

    health

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 5001')
